cd2f8a0c759ca258f36f7f52bab7598e3ff66762
Saransh <saransh.ahuja@student.ufv.ca>
2021-10-30 13:42:08 +0530

    Starting pages have been created //use the pub get in pubspec.yaml file

---
+++ lib/Screens/home_Screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Bits and Bites"),),
+      body: Column(
+        children: [
+          Text("Coding to be done here")
+        ],
+      ),
+    );
+  }
+}
