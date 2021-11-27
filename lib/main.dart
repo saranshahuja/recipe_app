c9f57fee60e9c636cd614ebb1b198a2035b9493d
Saransh <saransh.ahuja@student.ufv.ca>
2021-11-27 18:00:46 +0530

    DO NOT CHANGE THE MAIN FILE, WORK IN OTHER FILES OTHERWISE IT'LL MESS WITH THE PROVIDERS AND THE STREAMS!!!

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,15 @@ import 'package:recipe_app/Screens/home_Screen.dart';
 import 'Screens/home_Screen.dart';
 
 void main() {
-  runApp(const MyApp(
-  debugShowCheckedModeBanner: false,
-  home: MyApp(),
-  theme:   ,
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   ///this is saransh
-/// this is josh
-/// This is jeet
+  /// this is josh
+  /// This is jeet
   /// This is Shubham Rishi
   // This widget is the root of your application.
   @override
@@ -25,35 +22,13 @@ class MyApp extends StatelessWidget {
   ///DO NOT CHANGE THE MAIN FILE!!!!!!
   Widget build(BuildContext conext) {
     return MaterialApp(
-  title: 'Recipe App',
-  theme: ThemeData(
-    primartSwatch: Colors.blue,),
-  appBar: AppBar(
-    elevation: 0,
-    title: Text("Recipe App"),
+        title: 'Recipe App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,),
+        //appBar
+        home: const HomeScreen()
 
-      ), //appBar
-      home: const HomeScreen(),
-      //setting up search bar
-        body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical 10),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20)
-                ),//border
-                fillColor: Colors.green.withOpacity(0.05),
-                filled: true
-              ), //InputDecoration
-            ) //Textfield
-            SizedBox(
-              height:15,
-            ), //sizedbox
-          ], //children
-        ), //column
-      ),// container
     ); //MaterialApp
   }
+}
 
