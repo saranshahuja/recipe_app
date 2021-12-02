6fbdd05ea7554c387b04c3da84ec389995134044
Shubham Rishi (300159425) <Shubham.Rishi@student.ufv.ca>
2021-12-02 00:30:31 -0800

    search bar delegate setup

--- lib/Widgets/app_bar.dart
+++ lib/Widgets/app_bar.dart
@@ -1,35 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/delegates/search_recipes_delegate.dart';
 
-Widget appBar( BuildContext context, GlobalKey<ScaffoldState> key ) {
+Widget appBar(BuildContext context, GlobalKey<ScaffoldState> key) {
   return SliverAppBar(
     backgroundColor: Color.fromRGBO(244, 252, 255, 1.0),
     floating: false,
     leading: GestureDetector(
-      onTap:() {
+      onTap: () {
         key.currentState!.openDrawer();
       },
       child: Container(
         padding: EdgeInsets.all(15.0),
         child: Image(
           image: AssetImage('assets/menu.png'),
-          width:10.0,
+          width: 10.0,
           height: 10.0,
         ),
       ),
     ),
     actions: <Widget>[
       Container(
-        padding: EdgeInsets.all(15.0),
-        child:IconButton(icon: Icon(
-          Icons.search,
-          color: Color.fromRGBO(183, 195, 206, 1.0),
-          size: 25.0,
-        ),
-          onPressed: () {
-
-          }
-        )
-      )
-      ],
+          padding: EdgeInsets.all(15.0),
+          child: IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Color.fromRGBO(183, 195, 206, 1.0),
+                size: 25.0,
+              ),
+              onPressed: () {
+                showSearch(context: context, delegate: RecipeSearchDelegate());
+              }))
+    ],
   );
 }
