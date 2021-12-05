4cd3f95b630e2b3c799ad98d44b70de963dcb497
Diptyajeet.Ray <diptyajeet.ray@student.ufv.ca>
2021-12-05 05:47:49 -0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/Screens/home_Screen.dart';
-
+//this is saransh
+//this is josh
+// This is jeet
+// This is Shubham Rishi
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  ///this is saransh
-/// this is josh
-/// This is jeet
-  /// This is Shubham Rishi
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext conext) {
@@ -18,8 +18,9 @@ class MyApp extends StatelessWidget {
       title: 'Recipe App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const HomeScreen(),
+      home: Container(color: Colors.red),
     );
   }
 }
