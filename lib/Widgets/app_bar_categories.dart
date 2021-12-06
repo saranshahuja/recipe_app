b0cf890b53fa29acc04f64ecf08d167055ff2776
Diptyajeet.Ray <diptyajeet.ray@student.ufv.ca>
2021-12-06 08:14:20 -0800

    update to change widget name in app bar categories

--- lib/Widgets/app_bar_categories.dart
+++ lib/Widgets/app_bar_categories.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/Styles/styles.dart';
 
-Widget appBarCategoria(String title) {
+Widget appBarCategories(String title) {
   return SliverAppBar(
       backgroundColor: colorBG,
       floating: false,
