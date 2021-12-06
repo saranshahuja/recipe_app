d98711f2e9f0b9e057bd64c22620831419d3ebab
joshedge <joshedge04@gmail.com>
2021-12-06 11:53:57 -0800

    Added swiper_ingredients.dart.

---
+++ lib/Widgets/swiper_ingredients.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+final controller = PageController(
+  initialPage: 1,
+  viewportFraction: 0.3
+);
+
+Widget sliderIngredients () {
+  return Container(
+    height: 120.0,
+    padding: EdgeInsets.only(left:5.0),
+    child: Swiper(
+      itemCount: 3,
+      layout: SwiperLayout.DEFAULT,
+      itemBuilder: ( BuildContext context, int index ) {
+        return PageView(
+          controller: controller,
+          children: [
+            _imagen(),
+            _imagen(),
+            _imagen(),
+            _imagen()
+          ]
+        );
+      }
+    )
+  );
+}
+
+Widget _imagen() {
+  return Center(
+    child: Image(
+      alignment: Alignment.topLeft,
+      image: AssetImage('assets/ingredients.png'),
+    )
+  );
+}
