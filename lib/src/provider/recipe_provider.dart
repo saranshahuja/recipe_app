61f99e4e5f7d9d3cd6aad2801ea5d05102155e2a
Jahnavi <57895420+Jahnavi-Chawla@users.noreply.github.com>
2021-12-05 21:17:45 -0800

    Create recipe_provider.dart

---
+++ lib/src/provider/recipe_provider.dart
@@ -0,0 +1,18 @@
+import '';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+
+class _RecipeProvider {
+  List<dynamic> recipePopular = [];
+  
+  Future<List<dynamic>> cargarRecetasPopulares) async {
+    final resp = await rootBundle.loadString('data/recetas.json');
+    Map<String, dynamic> recipeMap = json. decode (resp);
+    recipePopular = recipeMap'recipePopular'];
+    
+    return recipePopular;
+  }
+
+}
+
+final recipeProvider - _RecipeProvider();
