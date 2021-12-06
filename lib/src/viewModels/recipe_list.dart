f10027fc880bdf81224386577e478791945de440
Jahnavi <57895420+Jahnavi-Chawla@users.noreply.github.com>
2021-12-05 19:40:39 -0800

    Update recipe_list.dart

--- lib/src/viewModels/recipe_list.dart
+++ lib/src/viewModels/recipe_list.dart
@@ -1,2 +1,31 @@
 import 'package:flutter/material.dart';
 
+List<Widget> recipeList( BuildContext context, List<dynamic> recipesPopular ) {
+  final List<Widget> listRecipe = [];
+  
+  recipePopular.forEach(( recipe ){
+    final listWidgetProv =  
+  });
+ 
+}
+
+Widget Recipe(BuildContext context) {
+  return Column(
+    children: <Widget>[
+      SizedBox(
+        height: 20.0,
+      ),
+      GestureDetector(
+        onTap: () {
+          Navigator.pushedNamed( context, 'detail'); 
+        },
+        child: Image(
+          image: AssetImage('assets/pasta.jpg'),
+          fit: BoxFit.fill,
+          width: 380.0,
+          height: 140.0,
+        ), //Image
+      ), //GestureDetector
+      textoRecipe( titlesRecipeStyle )
+      ], //<Widget>[]
+    ); //Column
