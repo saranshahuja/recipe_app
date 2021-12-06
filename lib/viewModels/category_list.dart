0a65471318d7161bcde652ad7521723b84f7bc46
Jahnavi <57895420+Jahnavi-Chawla@users.noreply.github.com>
2021-12-05 23:26:05 -0800

    Update category_list.dart

--- lib/viewModels/category_list.dart
+++ lib/viewModels/category_list.dart
@@ -20,8 +20,30 @@ Widget _imageCategory( BuildContext context, Map<String dynamic> category ) {
     onTap: () {
       Navigator.pushedNamed(context, 'category');
     },
-    child: Container(
-      child: Image(image: NetworkImage(category['photo'])),
-    ), //Container
+    child: Stack(
+      children: [
+        Container(
+          width: 130.0,
+          height: 100.0,
+          margin: EdgeInsets.only(right: 10.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image(
+              image: networkImage(category['photo']),
+              fit: BoxFit.cover,
+              ), //Image
+            )), //ClipRRect //Container
+        Container(
+          alignment: Alignment.bottomLeft,
+          padding: EdgeInsets.all(20.0),
+          child: Text(Category['Name'],
+                      style: textStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        decorationColor: Color.black,
+                        decorationStyle: TextDecorationStyle.wavy)), // TextStyle // Text
+          ) // Controller
+        ],
+      ), //Stack
     ); //GestureDetector
 }
