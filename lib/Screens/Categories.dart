4996384d1f228642515448184a6a052e1415bef8
Shubham Rishi (300159425) <Shubham.Rishi@student.ufv.ca>
2021-12-07 14:30:19 -0800

    category page setup, changes to be made in categories.dart file yet

--- lib/Screens/Categories.dart
+++ lib/Screens/Categories.dart
@@ -2,17 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:recipe_app/Styles/styles.dart';
 import 'package:recipe_app/Widgets/app_bar.dart';
 import 'package:recipe_app/Widgets/menu_lateral.dart';
+import 'package:recipe_app/Providers/recipes_provider.dart';
 
 class CategoriesPage extends StatelessWidget {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
+
+    final Object? categories = ModalRoute.of(context)!.settings.arguments;
+
     return Scaffold(
       drawer: menuLateral(context),
       key: _scaffoldKey,
       backgroundColor: colorBG,
-      body: CustomScrollView(slivers: [appBar(context, _scaffoldKey)]),
+      body: CustomScrollView(slivers: <Widget>[appBar(context, _scaffoldKey),
+        FutureBuilder(
+            future: RecipeProvider.carryRecipeCategories(categories['name']),
+            initialData: [],
+            builder: (BuildContext context, ){
+              return ;
+            }
+
+        )
+
+      ]
+      ),
     );
   }
 }
