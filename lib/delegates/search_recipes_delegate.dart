2d98ef38def633f722d55d664ee14a14f8fdf122
Shubham Rishi (300159425) <shubham.rishi@student.ufv.ca>
2021-12-07 00:54:28 -0800

    search recipe delegate setup and clean code

--- lib/delegates/search_recipes_delegate.dart
+++ lib/delegates/search_recipes_delegate.dart
@@ -4,35 +4,27 @@ import 'package:recipe_app/Providers/recipes_provider.dart';
 import 'package:recipe_app/viewModels/recipes_wanted_view_model.dart';
 
 class RecipeSearchDelegate extends SearchDelegate {
-
   @override
   ThemeData appBarTheme(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return theme.copyWith(
-      primaryColor: theme.primaryColorLight,
-      primaryIconTheme: theme.primaryIconTheme,
-      primaryColorBrightness: theme.primaryColorBrightness,
-      primaryTextTheme: theme.primaryTextTheme,
-      textTheme: theme.primaryTextTheme,
-      inputDecorationTheme: theme.inputDecorationTheme.copyWith(
-        hintStyle: TextStyle(
-          color: Colors.white60
-        )
-      )
-    );
+        primaryColor: theme.primaryColorLight,
+        primaryIconTheme: theme.primaryIconTheme,
+        primaryColorBrightness: theme.primaryColorBrightness,
+        primaryTextTheme: theme.primaryTextTheme,
+        textTheme: theme.primaryTextTheme,
+        inputDecorationTheme: theme.inputDecorationTheme
+            .copyWith(hintStyle: TextStyle(color: Colors.white60)));
   }
-
-
 
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
-    IconButton(
-        onPressed: () {
-          this.query = '';
-        }
-        ,icon: Icon(Icons.clear)
-    )
+      IconButton(
+          onPressed: () {
+            this.query = '';
+          },
+          icon: Icon(Icons.clear))
     ];
   }
 
@@ -41,43 +33,40 @@ class RecipeSearchDelegate extends SearchDelegate {
     return IconButton(
         onPressed: () {
           this.close(context, null);
-        }
-        ,icon: Icon(Icons.arrow_back_ios)
-    );
+        },
+        icon: Icon(Icons.arrow_back_ios));
   }
 
   @override
   Widget buildResults(BuildContext context) {
     return FutureBuilder(
-        future: RecipeProvider.loadPopularRecipes(),
-        initialData: [],
-        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot){
-          List<dynamic>? recipes = snapshot.data;
-          var recipeWanted = this.query.toLowerCase();
+      future: RecipeProvider.loadPopularRecipes(),
+      initialData: [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        List<dynamic>? recipes = snapshot.data;
+        var recipeWanted = this.query.toLowerCase();
 
-          return CustomScrollView(
-            slivers: [
-  SliverList(delegate: SliverChildListDelegate(
-    [
-      Column(
-        children: recipeListWanted(context, recipes!, recipeWanted),
-      )
-    ]
-  ))
-            ],
-          );
-        },
+        return CustomScrollView(
+          slivers: [
+            SliverList(
+                delegate: SliverChildListDelegate([
+              Column(
+                children: recipeListWanted(context, recipes!, recipeWanted),
+              )
+            ]))
+          ],
+        );
+      },
     );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-   return Container(
-     padding: EdgeInsets.all(10.0),
-     child: Text(
-       'Your Favourite Recipe...',
-       style: descriptionRecipeStyle,
-     )
-   );
+    return Container(
+        padding: EdgeInsets.all(10.0),
+        child: Text(
+          'Your Favourite Recipe...',
+          style: descriptionRecipeStyle,
+        ));
   }
 }
