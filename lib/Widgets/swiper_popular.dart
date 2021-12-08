e348b87bb59c88e55464014f80df8ecd54ebe9e1
Diptyajeet.Ray <diptyajeet.ray@student.ufv.ca>
2021-12-08 06:39:23 -0800

    tried adding a picture

--- lib/Widgets/swiper_popular.dart
+++ lib/Widgets/swiper_popular.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_swiper/flutter_swiper.dart';
 
 class swiperPopular extends StatelessWidget {
-
-  final controller = PageController(
-    initialPage: 1,
-    viewportFraction: 1
-  );
+  final controller = PageController(initialPage: 1, viewportFraction: 1);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 250.0,
       child: Swiper(
-        itemBuilder: ( BuildContext context, int index ) {
+        itemBuilder: (BuildContext context, int index) {
           return PageView(
-            children: <Widget>[
-              _target(),
-              _target(),
-              _target(),
-              _target()
-
-            ]
-          );
+              children: <Widget>[_target(), _target(), _target(), _target()]);
         },
         itemCount: 1,
         layout: SwiperLayout.DEFAULT,
@@ -36,11 +24,9 @@ class swiperPopular extends StatelessWidget {
       margin: EdgeInsets.all(10.0),
       alignment: AlignmentDirectional.center,
       child: Image(
-        image: AssetImage('assets/recipeSlider1.png'),
+        image: AssetImage('assets/recipeSlider1.jpeg'),
         fit: BoxFit.fill,
       ),
     );
   }
-
-
 }
