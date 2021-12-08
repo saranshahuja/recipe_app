93b7a4733aa2ace8b253818909a23c290634a159
Shubham Rishi (300159425) <Shubham.Rishi@student.ufv.ca>
2021-12-07 16:22:50 -0800

    ingredients swiper setup

--- lib/Widgets/swiper_ingredients.dart
+++ lib/Widgets/swiper_ingredients.dart
@@ -1,39 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:recipe_app/viewModels/Ingredients_list.dart';
 
-final controller = PageController(
-  initialPage: 1,
-  viewportFraction: 0.3
-);
 
-Widget sliderIngredients (List<dynamic> ingredients) {
+
+Widget sliderIngredients( List<dynamic> ingredients ) {
   final controller = PageController(initialPage: 1, viewportFraction: 0.3);
+
   return Container(
     height: 120.0,
-    padding: EdgeInsets.only(left:5.0),
+    padding: EdgeInsets.only(left: 5.0),
     child: Swiper(
-      itemCount: 3,
-      layout: SwiperLayout.DEFAULT,
-      itemBuilder: ( BuildContext context, int index ) {
-        return PageView(
-          controller: controller,
-          children: [
-            _image(),
-            _image(),
-            _image(),
-            _image()
-          ]
-        );
-      }
-    )
+        itemCount: 1,
+        layout: SwiperLayout.DEFAULT,
+        itemBuilder: (BuildContext context, int index) {
+          return PageView(
+            controller: controller,
+            children: IngredientsList( ingredients ),
+          );
+        }),
   );
 }
 
-Widget _image() {
-  return Center(
-    child: Image(
-      alignment: Alignment.topLeft,
-      image: AssetImage('assets/ingredients.png'),
-    )
-  );
-}
+
+
+
+
+
+// final controller = PageController(
+//   initialPage: 1,
+//   viewportFraction: 0.3
+// );
+
+// Widget sliderIngredients (List<dynamic> ingredients) {
+//   final controller = PageController(initialPage: 1, viewportFraction: 0.3);
+//   return Container(
+//     height: 120.0,
+//     padding: EdgeInsets.only(left:5.0),
+//     child: Swiper(
+//       itemCount: 3,
+//       layout: SwiperLayout.DEFAULT,
+//       itemBuilder: ( BuildContext context, int index ) {
+//         return PageView(
+//           controller: controller,
+//           children: [
+//             _image(),
+//             _image(),
+//             _image(),
+//             _image()
+//           ]
+//         );
+//       }
+//     )
+//   );
+// }
+//
+// Widget _image() {
+//   return Center(
+//     child: Image(
+//       alignment: Alignment.topLeft,
+//       image: AssetImage('assets/ingredients.png'),
+//     )
+//   );
+// }
